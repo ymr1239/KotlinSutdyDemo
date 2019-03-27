9706b32bc0b8f974b60e79b50815dc1696fee08d
ymr1239 <1729877052@qq.com>
2019-03-27 12:46:00 +0800

    做一个小小的测试

--- app/src/main/java/com/wljr/kotlinsutdydemo/MainActivity.kt
+++ app/src/main/java/com/wljr/kotlinsutdydemo/MainActivity.kt
@@ -8,5 +8,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sum(6,76)
+    }
+
+    fun sum(a:Int,b:Int){
+        println("sum of $a and $b is ${a+b}")
     }
 }
