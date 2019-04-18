595a78aeeb168097d01842eb2c11fc1658b0cdb2
ymr1239 <1729877052@qq.com>
2019-04-18 16:55:26 +0800

    no change

--- app/src/main/java/com/wljr/kotlinsutdydemo/detail/ClassAndObject/ClassDescription.kt
+++ app/src/main/java/com/wljr/kotlinsutdydemo/detail/ClassAndObject/ClassDescription.kt
@@ -25,17 +25,29 @@ class ClassDescription {
     //在实例初始化期间，初始化块按照它们出现在类体中的顺序执行，与属性初始化器交织在一起：
     class InitOrderDemo(name: String) {
         val firstProperty = "First property: $name".also(::println)
-
         init {
             println("First initializer block that prints ${name}")
         }
+        val secondProperty = "Second property: ${name.length}".also(::println)
 
-
-
-//        val secondProperty = "Second property: ${name.length}".also(::println)
-//
-//        init {
-//            println("Second initializer block that prints ${name.length}")
-//        }
+        init {
+            println("Second initializer block that prints ${name.length}")
+        }
     }
+
+    /**
+     * 次构造函数
+     * 如果类有一个主构造函数，每个次构造函数需要委托给主构造函数，
+     * 可以直接委托或者通过别的次构造函数间接委托。委托到同一个类的另一个构造函数用 this 关键字即可：
+     */
+
+    class Person(val name: String) {
+        constructor(name: String, parent: Person) : this(name) {
+            //parent.children.add(this)
+        }
+    }
+    //请注意，初始化块中的代码实际上会成为主构造函数的一部分。
+    // 委托给主构造函数会作为次构造函数的第一条语句，因此所有初始化块中的代码都会在次构造函数体之前执行。
+    // 即使该类没有主构造函数，这种委托仍会隐式发生，并且仍会执行初始化块：
+
 }
--- app/src/main/java/com/wljr/kotlinsutdydemo/detail/ClassAndObject/test.java
+++
@@ -1,7 +0,0 @@
-package com.wljr.kotlinsutdydemo.detail.ClassAndObject;
-
-public class test {
-    public static void main(String[] args) {
-
-    }
-}
